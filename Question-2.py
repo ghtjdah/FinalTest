99548ed3dabc5f7fcbf1e219c3c4020fd0f0772f
ghtjdah <ghtjday@naver.com>
2018-09-21 21:40:08 +0900

    2nd Commit - modified code (Quenstion-2)

--- Question-2.py
+++ Question-2.py
@@ -5,13 +5,19 @@ def randomgame():
     print('1-100 사이의 숫자 입력')
     count=1
     num=0
+    max = 100
+    min = 1
     restart=''
     while(True):
         num=int(input('{}>>'.format(count)))
         if(num < randomnum):
             print("더 높게")
+            min=num
+            print("{0} - {1}".format(min,max))
         elif(num > randomnum):
             print("더 낮게")
+            max=num
+            print("{0} - {1}".format(min, max))
         else:
             print("맞았습니다!")
             restart = input("다시 하시겠습니까?(y/n) >> ")
