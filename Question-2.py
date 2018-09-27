e8356b1d3f551726e9ac2d983d5be55b9ccff583
ghtjdah <ghtjday@naver.com>
2018-09-27 19:34:56 +0900

    4th Commit - modified code (Quenstion-2)

--- Question-2.py
+++ Question-2.py
@@ -1,31 +1,30 @@
 # 2번 문제
+import random
 
-def randomgame():
-    randomnum=int(input('임의의 수를 정했습니다 맞춰보세요 '))
+
+#___main___
+while (True):
+    min,max,trycount,num = 1,100,1,0
+    n=random.randrange(max)+1
+    print('임의의 수를 정했습니다 맞춰보세요!', n)
     print('1-100 사이의 숫자 입력')
-    count=1
-    num=0
-    max = 100
-    min = 1
     restart=''
     while(True):
-        num=int(input('{}>>'.format(count)))
-        if(num < randomnum):
+        num=int(input('{}>>'.format(trycount)))
+        if(num < n):
             print("더 높게")
             min=num
             print("{0} - {1}".format(min,max))
-        elif(num > randomnum):
+        elif(num > n):
             print("더 낮게")
             max=num
             print("{0} - {1}".format(min, max))
         else:
             print("맞았습니다!")
-            restart = input("다시 하시겠습니까?(y/n) >> ")
-            if(restart == 'y'):
-                randomgame()
-            elif(restart == 'n'):
-                break
-        count+=1
-
-#___main___
-randomgame()
+            break
+        trycount += 1
+    restart = input("다시 하시겠습니까?(y/n) >> ")
+    if(restart.lower() == 'y'):
+        continue
+    elif(restart.lower() == 'n'):
+        break
